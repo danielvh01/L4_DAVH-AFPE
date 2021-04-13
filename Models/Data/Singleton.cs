38bf51338b24ad7f3fb7af34092a91e08b18efd3
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-04-13 17:01:02 -0600

    Singleton

---
+++ Models/Data/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L4_DAVH_AFPE.Models.Data
+{
+    public sealed class Singleton
+    {
+        
+        private readonly static Singleton _instance = new Singleton();
+        
+        private Singleton()
+        {
+            
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public string keyGen(string date)
+        {
+            return "";
+        }
+    }
+}
