eeff0def718f908d7563454c569fa05936aac290
danielvh01 <danielvelasquez32@gmail.com>
2021-04-13 16:20:24 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace L4_DAVH_AFPE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
