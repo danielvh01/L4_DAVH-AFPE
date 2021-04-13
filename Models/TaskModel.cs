6e2c4f374f31bf0690590bf5a27c7be17275382c
danielvh01 <danielvelasquez32@gmail.com>
2021-04-13 17:16:39 -0600

    Model and Task view

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace L4_DAVH_AFPE.Models
 {
     public class TaskModel
     {
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public string project { get; set; }
+
+        [Required]
+        public int priority { get; set; }
+
+        [Required]
+        public string date { get; set; }
+
+        public string inCharge { get; set; }
     }
 }
