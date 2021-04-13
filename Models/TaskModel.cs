dadc4caccf0f773342cf78dfd7cf5a252d9d41ba
danielvh01 <danielvelasquez32@gmail.com>
2021-04-13 16:51:16 -0600

    Model

---
+++ Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L4_DAVH_AFPE.Models
+{
+    public class TaskModel
+    {
+    }
+}
